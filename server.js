94fd8bcb5a0eca65b167b59e371d7f0a9d36eb11
MaarLop <s.mariel.lopez1995@gmail.com>
2020-10-21 22:41:35 -0300

    Se cambio nombre de archivo server

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const PORT = process.env.PORT || 4020;
 
 var app_path = '../desapp-grupoo-frontend'
 
-app.use('/', express.static(path.join(__dirname, app_path )))
-	.listen(PORT, () =>	console.log(`Listening on port ${PORT}`))
-	.get('/*', function(req,res){
-		res.sendFile(path.join(__dirname + '/src/index.html'))
-	})
+app.use('/', express.static(__dirname, app_path ));
+
+app.listen(PORT, () =>	console.log(`Listening on port ${PORT}`));
+
+app.get('/*', function(req,res){
+	res.sendFile(path.join(__dirname + '/src/index.html'))
+});
