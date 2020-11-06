fab18378881cfa1445ae1936067ce1815fdfd182
Martin <castello.martin@yahoo.com.ar>
2020-11-06 00:37:10 -0300

    Initial auth0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './dashboard/user/user.component';
 import { DonationComponent } from './dashboard/donation/donation.component';
 import { LoginComponent } from './login/login.component';
-import { AuthButtonComponent } from './login/authButton.component';
 import { ResgisterComponent } from './resgister/resgister.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {path:'login', component : LoginComponent},
- {path:'dashboard', component : AuthButtonComponent},
+ {path:'dashboard', component : DashboardComponent},
  {path:'user', component : UserComponent},
  {path:'resgister', component : ResgisterComponent},
  {path:'donation', component : DonationComponent},
