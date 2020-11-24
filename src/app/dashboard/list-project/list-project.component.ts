1a016ec96f9c1bf55aab9d147ad31c3e7f4cbc26
Gisele Escobar <escobargisele1@gmail.com>
2020-11-24 19:59:06 -0300

    refactor en list-project.ts

--- src/app/dashboard/list-project/list-project.component.ts
+++ src/app/dashboard/list-project/list-project.component.ts
@@ -112,34 +112,6 @@ export class ListProjectComponent implements OnInit {
     localStorage.setItem('idProject', idProject);
     this.router.navigate(['/donation']);
   }
-  /*
-   //variable para la carga de un proyecto
-  project = '';
-  //list iria con la clase nuestra 
-  listProject: any[]=[];
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-  //metodo para imprimir el proyecto
-  addProject(){
-
-    const project={
-        name: this.project,
-        finishProject:false
-
-    };
-    console.log(this.listProject);
-    this.listProject.push(project);
-    this.project='';
-  };
-  //aca pongo index:num , pero en el proyecto nuestro cada proyecto ya trae un id q lo identifica
-  deleteProject( index :number ){
-    this.listProject.splice(index,1);
-
-  };
-  updateProyect(project,index){
-    this.listProject[index].finishProject =!project.finishProject;
-  }*/ 
+   
 }
 
